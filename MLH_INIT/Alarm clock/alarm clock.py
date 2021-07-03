b551f06d467fea94c48926f6d76bab34d702b791
Aarati Shah <64316366+Antlia360@users.noreply.github.com>
2021-07-03 11:36:41 +0530

    Add files via upload

---
+++ MLH_INIT/Alarm clock/alarm clock.py
@@ -0,0 +1,13 @@
+import datetime
+alarmH = int(input("What hour do you want the alarm to ring? "))
+alarmM = int(input("What minute do you want the alarm to ring? "))
+amPm = str(input("am or pm? "))
+if (amPm == "pm"):
+    alarmH = alarmH + 12
+while(1 == 1):
+    if(alarmH == datetime.datetime.now().hour and
+        alarmM == datetime.datetime.now().minute) :
+         print("Time to wake up")
+         break
+print('Excited')
+        
